adb7ebc9ece0764546e1a33e247c47bc5e248bff
admin <admin@192.168.1.6>
2021-04-28 17:48:26 +0530

    branch one

--- JavaProject/src/Hello/Arrays1.java
+++ JavaProject/src/Hello/Arrays1.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class Arrays1 {
 
 	void printArray() {
-		String[] arrayOne= {"b","c","d"};
+		String[] arrayOne= {"b","a","d","a"};
 		
-//		for(String s:arrayOne)
-//		{
-//			System.out.println(s);
-//		}
+		System.out.println("Hello world ONLY IN BRANCH ONE");
+		for(String s:arrayOne)
+		{
+			System.out.println(s);
+		}
 		
 		Arrays.stream(arrayOne).forEach(temp->{
 			System.out.println(temp);
